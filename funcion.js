c432d2713c044a5afab8e29000172988c4c17578
Eric Herrera <erich_473@hotmail.com>
2023-02-21 01:13:19 -0300

    Curriculo Vitae procto

---
+++ funcion.js
@@ -0,0 +1,31 @@
+function cambiarFondoCV() {
+    let body = document.getElementsByTagName('body') [0];
+
+    body.style.background = '#f00';
+}
+
+function cambiarTelefono() {
+    document.getElementById ('cambioTelefono').innerHTML = "(256) 35687221"
+    document.getElementById ('cambioTelefono').style.background = '#48a1a1'
+}
+
+function cambiarLocalidad() {
+    document.getElementById ('cambioLocalidad').innerHTML = "Rio Cuarto - Cordoba - Argentina"
+    document.getElementById ('cambioLocalidad').style.background = '#48a1a1'
+}
+
+function cambiarDireccion() {
+    document.getElementById ('cambioDireccion').innerHTML = "Vicente Lopez 546"
+    document.getElementById ('cambioDireccion').style.background = '#48a1a1'
+}   
+
+function cambiarCodigoPostal() {
+    document.getElementById ('cambioCodigoPostal').innerHTML = "C.P: 5865"
+    document.getElementById ('cambioCodigoPostal').style.background = '#48a1a1'
+}
+
+function cambiarEmail() {
+    document.getElementById ('cambioEmail').innerHTML = "goku_dbz@ejemplo.com"
+    document.getElementById ('cambioEmail').style.background = '#48a1a1'
+}
+
